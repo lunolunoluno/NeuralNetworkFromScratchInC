b339a90340ce25b19d1bf25942ef9b54b03f6fdb
lunolunoluno <noellucien2001@gmail.com>
2025-12-15 16:33:54 +0100

    ReLU implemented

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INPUT_SIZE 4
+#define INPUT_SIZE 2
 #define LAYER1_NB_NEURONS 3
 #define LAYER2_NB_NEURONS 3 // this is also the output
 
@@ -29,6 +29,7 @@ int main()
         layer1_weights[i] = calloc(INPUT_SIZE, sizeof(float));
     }
     float *layer1_output = calloc(LAYER1_NB_NEURONS, sizeof(float));
+    float *layer1_relu = calloc(LAYER1_NB_NEURONS, sizeof(float));
 
     float *layer2_biases = calloc(LAYER2_NB_NEURONS, sizeof(float));
     float **layer2_weights = malloc(LAYER2_NB_NEURONS * sizeof(float *));
@@ -39,18 +40,17 @@ int main()
     float *layer2_output = calloc(LAYER2_NB_NEURONS, sizeof(float));
 
     // GIVE VARIABLES INITIAL VALUES
-    inputs[0] = 1.0;
-    inputs[1] = 2.0;
-    inputs[2] = 3.0;
-    inputs[3] = 2.5;
+    inputs[0] = -0.8326189893369458;
+    inputs[1] = -0.5538462048218106;
+    int label = 0;
 
-    float layer1_weights_values[INPUT_SIZE * LAYER1_NB_NEURONS] = {0.2, 0.8, -0.5, 1.0,
-                                                                   0.5, -0.91, 0.26, -0.5,
-                                                                   -0.26, -0.27, 0.17, 0.87};
+    float layer1_weights_values[INPUT_SIZE * LAYER1_NB_NEURONS] = {0.01764052, 0.02240893,
+                                                                   0.00400157, 0.01867558,
+                                                                   0.00978738, -0.00977278};
     set_weights_value(layer1_weights, INPUT_SIZE, LAYER1_NB_NEURONS, layer1_weights_values);
-    layer1_biases[0] = 2.0;
-    layer1_biases[1] = 3.0;
-    layer1_biases[2] = 0.5;
+    layer1_biases[0] = 0.2;
+    layer1_biases[1] = 0.003;
+    layer1_biases[2] = 0.0005;
 
     float layer2_weights_values[LAYER1_NB_NEURONS * LAYER2_NB_NEURONS] = {0.1, -0.14, 0.5,
                                                                           -0.5, 0.12, -0.33,
@@ -67,8 +67,10 @@ int main()
         for (int j = 0; j < INPUT_SIZE; j++)
         {
             neuron_output += inputs[j] * layer1_weights[i][j];
+            // printf("%f * %f = %f\n", inputs[j], layer1_weights[i][j], neuron_output);
         }
         neuron_output += layer1_biases[i];
+        // printf("+ %f = %f\n", layer1_biases[i], neuron_output);
         layer1_output[i] = neuron_output;
     }
 
@@ -78,6 +80,19 @@ int main()
         printf("%f,", layer1_output[i]);
     }
     printf("\n");
+
+    // LAYER 1 ReLU
+    for (int i = 0; i < LAYER1_NB_NEURONS; i++)
+    {
+        layer1_relu[i] = (layer1_output[i] > 0.0) ? layer1_output[i] : 0.0;
+    }
+
+    printf("LAYER 1 RELU: ");
+    for (int i = 0; i < LAYER1_NB_NEURONS; i++)
+    {
+        printf("%f,", layer1_relu[i]);
+    }
+    printf("\n");  
 
     // FEED FORWARD LAYER 2
     for (int i = 0; i < LAYER2_NB_NEURONS; i++)
@@ -107,6 +122,7 @@ int main()
     free(layer2_weights);
     free(layer2_biases);
 
+    free(layer1_relu);
     free(layer1_output);
     for (int i = 0; i < LAYER1_NB_NEURONS; i++)
     {
